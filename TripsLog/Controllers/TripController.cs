23be3927ac1085ac8ae6c67443b987ec1a996f13
Lawrence Splaver <lsplaver@yahoo.com>
2021-10-24 23:43:34 -0400

    Everything except the displayed url for page3 when routed to from page1 is working correctly

--- TripsLog/Controllers/TripController.cs
+++ TripsLog/Controllers/TripController.cs
@@ -27,7 +27,8 @@ namespace TripsLog.Controllers
             TempData.Clear();
             return RedirectToAction("Index");
         }
-        
+
+        [Route("/[controller]/[action]")]
         [HttpGet]
         public IActionResult Add()
         {
@@ -37,32 +38,91 @@ namespace TripsLog.Controllers
             return View("Page1", trip);
         }
 
-        [HttpPost]
-        public IActionResult AddPage1(int PageId, Trip trip)
+        [Route("/[controller]/[action]/page{id}")]
+        // [HttpGet]
+        public IActionResult Add(int id, Trip trip)
         {
+            // string strAction = "";
+            // switch((int)TempData["CurrentPage"])
+            // {
+            // case 1:
+            if ((int)TempData["CurrentPage"] == 1)
+            {
+                TempData["Destination"] = trip.Destination;
+                TempData["Accommodation"] = trip.Accommodation;
+                TempData["StartDate"] = trip.StartDate;
+                TempData["EndDate"] = trip.EndDate;
+                // ViewBag.Action = "Add";
+                if (TempData.Peek("Accommodation") != null)
+                {
+                    return View("Page2" /*, trip */);
+                }
+                else
+                {
+                    return View("Page3" /*, trip */);
+                }
+            }
+            // strAction = "AddPage1";
+            // break;
+            // case 2:
+            else if ((int)TempData["CurrentPage"] == 2)
+            {
+                TempData["AccommodationPhone"] = trip.AccommodationPhone;
+                TempData["AccommodationEmail"] = trip.AccommodationEmail;
+                return View("Page3" /*, trip */);
+            }
+            // strAction = "AddPage2";
+            // break;
+            // case 3:
+            else
+            {
+                trip.Destination = (string)TempData["Destination"];
+                trip.Accommodation = (string)TempData.Peek("Accommodation");
+                trip.StartDate = (DateTime)TempData["StartDate"];
+                trip.EndDate = (DateTime)TempData["EndDate"];
+                trip.AccommodationPhone = (string)TempData["AccommodationPhone"];
+                trip.AccommodationEmail = (string)TempData["AccommodationEmail"];
+                context.Trips.Add(trip);
+                context.SaveChanges();
+                TempData["UserMessage"] = "Trip to " + TempData["Destination"] + " added.";
+                return RedirectToAction("Index", "Home");
+            }
+                    // strAction = "AddPage3";
+                    // break;
+                    // default:
+                    // strAction = "AddNew";
+                    // break;
+            }
+            // return RedirectToAction(strAction, trip);
+        }
+
+        /* [HttpPost]
+        public IActionResult AddPage1(int PageId, Trip trip)
+        //
             TempData["Destination"] = trip.Destination;
             TempData["Accommodation"] = trip.Accommodation;
             TempData["StartDate"] = trip.StartDate;
             TempData["EndDate"] = trip.EndDate;
+            // ViewBag.Action = "Add";
             if (TempData.Peek("Accommodation") != null)
             {
-                return View("Page2", trip);
+                return View("Page2" /*, trip */ /* );
             }
             else
             {
                 return View("Page3", trip);
             }
-        }
+        } */
 
-        [HttpPost]
+        /* [HttpPost]
         public IActionResult AddPage2(Trip trip)
         {
             TempData["AccommodationPhone"] = trip.AccommodationPhone;
             TempData["AccommodationEmail"] = trip.AccommodationEmail;
             return View("Page3", trip);
-        }
+        } */ 
 
-        [HttpPost]
+        /* [HttpPost]
         public IActionResult AddPage3(Trip trip)
         {
             trip.Destination = (string)TempData["Destination"];
@@ -75,6 +135,6 @@ namespace TripsLog.Controllers
             context.SaveChanges();
             TempData["UserMessage"] = "Trip to " + TempData["Destination"] + " added.";
             return RedirectToAction("Index", "Home");
-        }
+        } */
     }
-}
+// }
